806914546d82c44b2267d9f0e95e21d989b4915f
ronnilenvighansen <ronnilenvighansen@gmail.com>
2024-09-13 10:42:28 +0100

    Add files via upload

---
+++ ConsoleSearch/SearchLogic.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+
+namespace ConsoleSearch
+{
+    public class SearchLogic
+    {
+        HttpClient api = new() { BaseAddress = new Uri("http://localhost:5239") };
+
+        Dictionary<string, int> mWords;
+
+        /*public SearchLogic()
+        {
+            mWords = mDatabase.GetAllWords();
+        }*/
+
+        public int GetIdOf(string word)
+        {
+            if (mWords.ContainsKey(word))
+                return mWords[word];
+            return -1;
+        }
+
+        public Dictionary<int, int> GetDocuments(List<int> wordIds)
+        {
+            var url = "Document/GetByWordIds?wordIds=" + string.Join("&wordIds=", wordIds);
+            var response = api.Send(new HttpRequestMessage(HttpMethod.Get, url));
+            var content = response.Content.ReadAsStringAsync().Result;
+            return JsonSerializer.Deserialize<Dictionary<int, int>>(content);
+        }
+
+        public List<string> GetDocumentDetails(List<int> docIds)
+        {
+            var url = "Document/GetByDocIds?docIds=" + string.Join("&docIds=", docIds);
+            var response = api.Send(new HttpRequestMessage(HttpMethod.Get, url));
+            var content = response.Content.ReadAsStringAsync().Result;
+            return JsonSerializer.Deserialize<List<string>>(content);
+        }
+    }
+}
